78ba35de1a4d7cf165e88ff2bdefcdd9f648463c
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-18 15:24:37 +0100

    test express

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+// import { createConnection } from "typeorm";
+import express from "express";
+// import { ToDo } from "./entity/ToDo";
 
-import { ToDo } from "./entity/ToDo";
+(async () => {
+  const app = express();
+  app.get("/", (_req, res) => res.send("hello"))
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+})();
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new ToDo into the database...");
-    const std = new ToDo();
-    std.Title = "ToDo 1";
-    std.Description = "Clean the house";
-    await connection.manager.save(std);
-    console.log("Saved a new user with id: " + std.id);
+// createConnection()
+//   .then(async (connection) => {
+//     console.log("Inserting a new ToDo into the database...");
+//     const std = new ToDo();
+//     std.Title = "ToDo 1";
+//     std.Description = "Clean the house";
+//     await connection.manager.save(std);
+//     console.log("Saved a new user with id: " + std.id);
 
-    console.log("Loading ToDo List from the database...");
-    const todos = await connection.manager.find(ToDo);
-    console.log("Loaded ToDoList: ", todos);
+//     console.log("Loading ToDo List from the database...");
+//     const todos = await connection.manager.find(ToDo);
+//     console.log("Loaded ToDoList: ", todos);
 
-    console.log("TypeORM with MongoDB");
-  })
-  .catch((error) => console.log(error));
+//     console.log("TypeORM with MongoDB");
+//   })
+//   .catch((error) => console.log(error));
