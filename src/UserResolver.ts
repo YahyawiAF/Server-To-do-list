0cd4acb5f0d31e2d45145d5a14149ccc4bab1c23
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-18 16:36:23 +0100

    user resolvers

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,9 +1,34 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { User } from "./entity/User";
+import { hash } from "bcryptjs";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
     return "hi men!";
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
   }
 }
