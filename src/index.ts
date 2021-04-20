7f5461324cacab8962591a4328547ba7c58d5807
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-20 01:59:08 +0100

    refresh token final part

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,39 @@ import { createConnection } from "typeorm";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 // import { ToDo } from "./entity/ToDo";
-import { UserResolver } from "./UserResolver";
+import { UserResolver } from "./resolvers/UserResolver";
 import { buildSchema } from "type-graphql";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { sendRefreshToken } from "./sendRefreshToken";
+import { createAccessToken, createRefreshToken } from "./auth";
 
 (async () => {
   const app = express();
+  app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello"));
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
   await createConnection();
   const appoloServer = new ApolloServer({
     schema: await buildSchema({
