77ef4abc153d7e9853be7a741a42cb29e63fc84a
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-20 01:59:45 +0100

    refactor Todo

--- src/entity/ToDo.ts
+++ src/entity/ToDo.ts
@@ -1,13 +1,30 @@
-import { Entity, ObjectID, ObjectIdColumn, Column, BaseEntity } from "typeorm";
+import {
+  Entity,
+  ObjectID,
+  ObjectIdColumn,
+  Column,
+  BaseEntity,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { ObjectType, Field } from "type-graphql";
+import { User } from "./User";
 
-@Entity("users")
+@ObjectType()
+@Entity("todos")
 export class ToDo extends BaseEntity {
   @ObjectIdColumn()
   id: ObjectID;
 
+  @Field()
   @Column()
   Title: string;
 
+  @Field()
   @Column()
   Description: string;
+
+  @ManyToOne(() => User, (user: User) => user.todos)
+  @JoinColumn({ name: "user_email" })
+  user: string;
 }
