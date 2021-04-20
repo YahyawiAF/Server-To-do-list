aae9ece8f5ef67c4c183d9efbaebc7aa0009544c
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-20 16:23:15 +0100

    add delete update todos

--- src/resolvers/TodoResolvers.ts
+++ src/resolvers/TodoResolvers.ts
@@ -3,7 +3,6 @@ import {
   Query,
   Arg,
   Mutation,
-  //   ObjectType,
   Field,
   Ctx,
   UseMiddleware,
@@ -11,27 +10,26 @@ import {
 } from "type-graphql";
 import { MyContext } from "../MyContext";
 import { isAuth } from "../isAuth";
-// import {User} from "../entity/User"
+import { getRepository } from "typeorm";
 import { ToDo } from "../entity";
 
 @InputType()
 export class CreateTodoInput {
-  @Field()
+  @Field({ nullable: true })
   Title: string;
-  @Field()
+  @Field({ nullable: true })
   Description: string;
 }
 
 @Resolver()
 export class ToDoResolver {
   @Query(() => [ToDo])
-  //@UseMiddleware(isAuth)
-  async todos(): Promise<any> {
+  @UseMiddleware(isAuth)
+  async todos(@Ctx() { payload }: MyContext): Promise<any> {
     try {
-      console.log("todo", await ToDo.find());
-      return ToDo.find();
+      return ToDo.find({ Creator: payload!.userEmail });
     } catch (err) {
-      console.log("err", err);
+      throw new Error("Cannot get todos");
     }
   }
 
@@ -47,6 +45,36 @@ export class ToDoResolver {
     todo.IsCompleted = false;
     todo.Creator = payload!.userEmail;
     await todo.save();
+    return todo;
+  }
+
+  @Mutation(() => ToDo)
+  @UseMiddleware(isAuth)
+  async updateToDo(
+    @Arg("id") id: string,
+    @Arg("todo") input: CreateTodoInput
+  ): Promise<CreateTodoInput> {
+    const todoRepository = getRepository(ToDo);
+    const todo = await todoRepository.findOne(id);
+    if (!todo) {
+      throw new Error("todo not found");
+    }
+    if (input.Title) todo.Title = input.Title;
+    if (input.Description) todo.Description = input.Description;
+    await todoRepository.save(todo);
+    return todo;
+  }
+
+  @Mutation(() => ToDo)
+  @UseMiddleware(isAuth)
+  async deleteToDo(@Arg("id") id: string): Promise<CreateTodoInput> {
+    const todoRepository = getRepository(ToDo);
+    const todo = await todoRepository.findOne(id);
+    if (!todo) {
+      throw new Error("todo not found");
+    }
+    const c = await todoRepository.delete(id);
+    console.log("c", c);
     return todo;
   }
 }
