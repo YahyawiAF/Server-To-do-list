00e962378326f365e755e5712c8e7b6e883d81e8
Yahyawi Abdel Fatteh <abdelfatteh.yahyawi@weavelines.com>
2021-04-26 02:34:23 +0100

    update myContext

--- src/MyContext.ts
+++ src/MyContext.ts
@@ -3,5 +3,5 @@ import { Request, Response } from "express";
 export interface MyContext {
   req: Request;
   res: Response;
-  payload?: { userEmail: string };
+  payload: { userEmail: string };
 }
